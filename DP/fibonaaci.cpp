240e984220f430c517139fb0a3ed3ef174817484
Shreya Singhal <shreyasinghal003@gmail.com>
2023-06-28 23:06:27 +0530

    Code added

--- DP/fibonaaci.cpp
+++ DP/fibonaaci.cpp
@@ -1,9 +1,28 @@
 #include <iostream>
+#include<bits/stdc++.h>
 using namespace std;
+
+// memoization 
+int fib(int n, int dp[]){
+if(n<=1){
+    return n;
+}
+if(dp[n]!=-1){
+return dp[n];
+}
+return dp[n] = fib(n-1, dp) + fib(n-2, dp);
+
+}
 
 int main(){
 int n;
 cin>>n;
+int dp[n+1];
+memset(dp,-1, sizeof(dp));
+cout<<fib(n,dp);
+
+
+// Tabulation method
 int *memo = new int[n];
 memo[0] = 0;
 memo[1] = 1;
@@ -15,3 +34,5 @@ memo[i] = memo[i-1] + memo[i-2];
 cout<<"Nth fibonaaci number: "<<memo[n-1]<<endl;
 return 0;
 }
+
+// Tabulation method: TC = O(N), SC = O(N)
