7f935e81f80e59d160bb2e782d6a439e8cc690f9
Shreya Singhal <shreyasinghal003@gmail.com>
2023-06-29 18:05:14 +0530

    Climbing stairs

---
+++ DP/climbingStairs.cpp
@@ -0,0 +1,47 @@
+// You are climbing a staircase. It takes n steps to reach the top.
+
+// Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+// Example 1:
+
+// Input: n = 2
+// Output: 2
+// Explanation: There are two ways to climb to the top.
+// 1. 1 step + 1 step
+// 2. 2 steps
+
+
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int climbStairs(int n) {
+        // memoization
+        int dp[n+1];
+        memset(dp, -1, sizeof(dp));
+        // if(n==0 || n==1){
+        //     return 1;
+        // }
+        // return dp[n] =  climbStairs(n-1) + climbStairs(n-2); 
+        // Tabulation
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for(int i= 2;i<=n;i++){
+            dp[i] =   dp[i-1] +  dp[i-2];
+        }
+    return dp[n];
+// recursive
+        // if(n==0){
+        //     return 1;
+        // }
+        // if(n==1){
+        //     return 1;
+        // }
+        // return climbStairs(n-1) + climbStairs(n-2); 
+    }
+
+int main(){
+int n;
+cin>>n;
+cout<<climbStairs(n);
+}
--- DP/fibonaaci.cpp
+++ DP/fibonaaci.cpp
@@ -2,14 +2,14 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-// memoization 
+// memoization  - Top Down approach
 int fib(int n, int dp[]){
 if(n<=1){
     return n;
-}
+ }
 if(dp[n]!=-1){
 return dp[n];
-}
+ }
 return dp[n] = fib(n-1, dp) + fib(n-2, dp);
 
 }
@@ -22,7 +22,7 @@ memset(dp,-1, sizeof(dp));
 cout<<fib(n,dp);
 
 
-// Tabulation method
+// Tabulation method - Bottom Up approach
 int *memo = new int[n];
 memo[0] = 0;
 memo[1] = 1;
